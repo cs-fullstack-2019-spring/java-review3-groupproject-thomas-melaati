8052193746968d05b635fca38edfd84519621e7d
MelaatiJ <fathimajayah876@gmail.com>
2019-05-23 15:07:52 -0500

    create user complete and create codecrew mail in progress

--- src/company/mail/Main.java
+++ src/company/mail/Main.java
@@ -1,8 +1,11 @@
 package company.mail;
-
+import company.mail.user;
+import company.mail.mail;
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        user.createNewUser();
+
     }
 }
--- src/company/mail/mail.java
+++ src/company/mail/mail.java
@@ -1,4 +1,45 @@
 package company.mail;
 
+import java.sql.*;
+import java.util.*;
+import java.util.Scanner;
+
 public class mail {
+    private final static String url = "jdbc:postgresql://138.197.107.95:5432/group1";
+    private final static String user = "student";
+    private final static String password = "C0d3Cr3w";
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+
+            testcodeCrewMailSQL(conn);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage( ));
+        }
+
+        return conn;
+    }
+
+    public static void testcodeCrewMailSQL(Connection conn){
+
+        try {
+
+            String SQL = "select * from codecrewmail";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while(rs.next()){
+                System.out.println(rs.getString("subject"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage( ));
+        }
+
+    }
+
+
 }
