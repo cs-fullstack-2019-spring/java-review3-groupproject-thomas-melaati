a10769a379c13b58c309eb97126d070ac9099c82
tdude0175 <tdude0175@gmail.com>
2019-05-24 10:24:36 -0500

    allows person to select user to send mail to

--- src/company/mail/user.java
+++ src/company/mail/user.java
@@ -10,16 +10,15 @@ public class user {
     private final static String url = "jdbc:postgresql://138.197.107.95:5432/group1";
     private final static String user = "student";
     private final static String password = "C0d3Cr3w";
+    public static Scanner scan = new Scanner(System.in);
 
 
     public static Connection connect() {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
+//            System.out.println("Connected to the PostgreSQL server successfully.");
 
-//            testmailmasterSQL(conn);
-//            createNewUser();
 
         } catch (SQLException e) {
             System.out.println(e.getMessage( ));
@@ -51,8 +50,10 @@ public class user {
         System.out.println("Create a new User");
         System.out.println("Enter name");
         String name = scan.nextLine();
+
         System.out.println("Enter Password");
         String password=scan.nextLine();
+
         System.out.println("Thankyou user created");
 
 
@@ -96,6 +97,44 @@ public class user {
             System.out.println(e.getMessage( ));
         }
         return 0;
+    }
+
+    public static int getUserToSendTo()
+    {
+        Connection conn = connect();
+
+            try {
+
+                String SQL = "select * from mailmaster";
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(SQL);
+
+                while(rs.next()){
+                    System.out.print(rs.getString(1)+"\t");
+                    System.out.println(rs.getString(2));
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage( ));
+            }
+            System.out.println("Who Do you want to send mail to?"+"\n");
+            int numberToReturn = scan.nextInt();
+        try {
+
+            String SQL = "select * from mailmaster";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(SQL);
+
+            while(rs.next()){
+                if(numberToReturn == rs.getInt(1)){
+                    return numberToReturn;
+                }
+                else return 0;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage( ));
+        }
+        return 0;
+
     }
 
 
